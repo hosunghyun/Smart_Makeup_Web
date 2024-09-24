e96725c0f13ab81c2893cd8df9daf7d34f0d5093
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-09-24 16:44:50 +0900

    Add files via upload

---
+++ smart_makeup_web/src/test/java/com/smwhc/smart_makeup_web/SmartMakeupWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smwhc.smart_makeup_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMakeupWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
