5ab43ba0a59c9e44c03454a295059f6e7e67f8d9
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-09-26 14:46:22 +0900

    DB/20240925/version_03
    SQL 파일 오류 수정과 글과 댓글 varchar에서 text로 변경

--- DB설계/smart_makeup_web.sql
+++ DB설계/smart_makeup_web.sql
@@ -1,54 +1,57 @@
-CREATE TABLE `User` (
-  `UserID` VARCHAR(20) PRIMARY KEY,
-  `Password` VARCHAR(20) NOT NULL,
-  `Email` VARCHAR(30) NOT NULL,
-  `PhoneNumber` VARCHAR(13)
+CREATE DATABASE smart_makeup;
+USE smart_makeup;
+
+CREATE TABLE user (
+    user_id VARCHAR(20) NOT NULL UNIQUE,
+    user_password VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    phone VARCHAR(13),
+    PRIMARY KEY(user_id)
 );
 
-CREATE TABLE `Makeup` (
-  `MakeupID` INT PRIMARY KEY,
-  `UserID` VARCHAR(20),
-  `ColorCode` VARCHAR(20) NOT NULL,  -- Assuming a hex color code
-  `Opacity` INT NOT NULL,
-  FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`) ON DELETE CASCADE
+CREATE TABLE makeup (
+	makeup_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id VARCHAR(20),
+	color_code VARCHAR(20) NOT NULL,
+	opacity INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE `ProductType` (
-  `MakeupID` INT,
-  `ProductCode` INT,
-  `ProductTypeName` VARCHAR(20) NOT NULL,
-  FOREIGN KEY (`MakeupID`) REFERENCES `Makeup` (`MakeupID`) ON DELETE CASCADE,
-  FOREIGN KEY (`ProductCode`) REFERENCES `Product` (`ProductCode`) ON DELETE CASCADE
+CREATE TABLE image_link (
+	image_code INT AUTO_INCREMENT PRIMARY KEY,
+	image_link VARCHAR(20) NOT NULL
+);
+CREATE TABLE product (
+	product_code INT AUTO_INCREMENT PRIMARY KEY,
+	image_code INT,
+	product_name VARCHAR(20) NOT NULL,
+	Price INT NOT NULL,
+	FOREIGN KEY (image_code) REFERENCES image_link (image_code) ON DELETE SET NULL
 );
 
-CREATE TABLE `Product` (
-  `ProductCode` INT PRIMARY KEY,
-  `ImageCode` INT,
-  `ProductName` VARCHAR(20) NOT NULL,
-  `Price DECIMAL(10, 2) NOT NULL,  -- Adjust precision as needed
-  FOREIGN KEY (`ImageCode`) REFERENCES `ImageLink` (`ImageCode`) ON DELETE SET NULL
+CREATE TABLE product_type (
+	makeup_id INT,
+	product_code INT,
+	product_type_name VARCHAR(20) NOT NULL,
+	FOREIGN KEY (makeup_id) REFERENCES makeup (makeup_id) ON DELETE CASCADE,
+	FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
 );
 
-CREATE TABLE `Board` (
-  `BoardID` INT PRIMARY KEY,
-  `UserID` VARCHAR(20),
-  `ImageCode` INT,
-  `Title` VARCHAR(20) NOT NULL,
-  `Content TEXT,
-  FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`) ON DELETE CASCADE,
-  FOREIGN KEY (`ImageCode`) REFERENCES `ImageLink` (`ImageCode`) ON DELETE SET NULL
+CREATE TABLE board (
+	board_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id VARCHAR(20),
+	image_code INT,
+    title VARCHAR(20) NOT NULL,
+    content_text TEXT,
+	FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
+	FOREIGN KEY (image_code) REFERENCES image_link (image_code) ON DELETE SET NULL
 );
 
-CREATE TABLE `Comment` (
-  `CommentID` INT PRIMARY KEY,
-  `BoardID` INT,
-  `UserID` VARCHAR(20),
-  `CommentContent` TEXT NOT NULL,
-  FOREIGN KEY (`BoardID`) REFERENCES `Board` (`BoardID`) ON DELETE CASCADE,
-  FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`) ON DELETE CASCADE
-);
-
-CREATE TABLE `ImageLink` (
-  `ImageCode` INT PRIMARY KEY,
-  `ImageLink` VARCHAR(20) NOT NULL
+CREATE TABLE comment (
+	comment_id INT AUTO_INCREMENT PRIMARY KEY,
+	board_id INT,
+    user_id VARCHAR(20),
+    comment_content TEXT NOT NULL,
+	FOREIGN KEY (board_id) REFERENCES board (board_id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
