f3e89d263ec0c9d5c3f051db9fc91d2b4ca5f7d8
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-09-27 13:05:28 +0900

    DB/20240925/version_04
    데이터베이스 변경 이미지와 게시판, 제품에 대한 관계 수정 일대다 -> 다대일

--- DB설계/smart_makeup_web.sql
+++ DB설계/smart_makeup_web.sql
@@ -1,10 +1,7 @@
-CREATE DATABASE smart_makeup;
-USE smart_makeup;
-
 CREATE TABLE user (
     user_id VARCHAR(20) NOT NULL UNIQUE,
     user_password VARCHAR(20) NOT NULL,
-    email VARCHAR(30) NOT NULL UNIQUE,
+    email VARCHAR(30) NOT NULL,
     phone VARCHAR(13),
     PRIMARY KEY(user_id)
 );
@@ -17,16 +14,31 @@ CREATE TABLE makeup (
 	FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE image_link (
-	image_code INT AUTO_INCREMENT PRIMARY KEY,
-	image_link VARCHAR(20) NOT NULL
+CREATE TABLE board (
+	board_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id VARCHAR(20),
+	-- image_code INT,
+    title VARCHAR(20) NOT NULL,
+    content_text TEXT,
+	FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
+	-- FOREIGN KEY (image_code) REFERENCES image_link (image_code) ON DELETE SET NULL
 );
+
 CREATE TABLE product (
 	product_code INT AUTO_INCREMENT PRIMARY KEY,
-	image_code INT,
+	-- image_code INT,
 	product_name VARCHAR(20) NOT NULL,
-	Price INT NOT NULL,
-	FOREIGN KEY (image_code) REFERENCES image_link (image_code) ON DELETE SET NULL
+	Price INT NOT NULL
+	-- FOREIGN KEY (image_code) REFERENCES image_link (image_code) ON DELETE SET NULL
+);
+
+CREATE TABLE image (
+	image_code INT AUTO_INCREMENT PRIMARY KEY,
+    board_id INT,
+    product_code INT,
+	image_link VARCHAR(20) NOT NULL,
+    FOREIGN KEY (board_id) REFERENCES board (board_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
 );
 
 CREATE TABLE product_type (
@@ -35,16 +47,6 @@ CREATE TABLE product_type (
 	product_type_name VARCHAR(20) NOT NULL,
 	FOREIGN KEY (makeup_id) REFERENCES makeup (makeup_id) ON DELETE CASCADE,
 	FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
-);
-
-CREATE TABLE board (
-	board_id INT AUTO_INCREMENT PRIMARY KEY,
-	user_id VARCHAR(20),
-	image_code INT,
-    title VARCHAR(20) NOT NULL,
-    content_text TEXT,
-	FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
-	FOREIGN KEY (image_code) REFERENCES image_link (image_code) ON DELETE SET NULL
 );
 
 CREATE TABLE comment (
