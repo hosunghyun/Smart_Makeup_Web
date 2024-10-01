f5cc7c010853cbd8b7453ea42205d352edfb6fac
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-10-01 10:45:14 +0900

    Delete smart_makeup_web/src/main/smart_makeup_web directory

--- smart_makeup_web/src/main/smart_makeup_web/Image/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smwhc.smart_makeup_web.Image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-    
-}
--- smart_makeup_web/src/main/smart_makeup_web/Makeup/MakeUpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smwhc.smart_makeup_web.Makeup;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MakeUpRepository extends JpaRepository<MakeUp, Long> {
-    
-}
