2514a4d500e87c99bb0ce50ff76387121603b8df
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-10-01 10:46:03 +0900

    Delete smart_makeup_web/src/main/java/com/smwhc/smart_makeup_web/Product directory

--- smart_makeup_web/src/main/java/com/smwhc/smart_makeup_web/Product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smwhc.smart_makeup_web.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    
-}
