7c6f9a3f86801bded5e95945ceafc985db1337d2
hyunhosung <hhs04321@naver.com>
2024-10-02 19:27:16 +0900

    smartmakeupweb/20241002/version_22

--- smart_makeup_web/src/main/java/com/smwhc/smart_makeup_web/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smwhc.smart_makeup_web.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-// 회원정보를 위한 레포지토리
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-    
-}
