5df794c6bb61eada7702a585f13733a35da0699a
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-10-04 11:08:18 +0900

    DB/20241004/version_10
    데이터베이스 키 값 범위 증가

--- DB설계/smart_makeup_web.sql
+++ DB설계/smart_makeup_web.sql
@@ -1,14 +1,14 @@
 CREATE TABLE member (
-    member_id VARCHAR(20) NOT NULL UNIQUE,
+    member_id VARCHAR(50) NOT NULL UNIQUE,
 	member_password VARCHAR(100) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    phone VARCHAR(13),
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(20),
     PRIMARY KEY(member_id)
 );
 
 CREATE TABLE makeup (
 	makeup_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-	member_id VARCHAR(20),
+	member_id VARCHAR(50),
 	color_code VARCHAR(20) NOT NULL,
 	opacity INT NOT NULL,
 	FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE
@@ -16,15 +16,15 @@ CREATE TABLE makeup (
 
 CREATE TABLE board (
 	board_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-	member_id VARCHAR(20) NOT NULL,
-    title VARCHAR(20) NOT NULL,
+	member_id VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     content_text TEXT,
 	FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE
 );
 
 CREATE TABLE product (
 	product_code BIGINT AUTO_INCREMENT PRIMARY KEY,
-	product_name VARCHAR(20) NOT NULL,
+	product_name VARCHAR(50) NOT NULL,
 	Price INT NOT NULL
 );
 
@@ -32,7 +32,7 @@ CREATE TABLE image (
 	image_code BIGINT AUTO_INCREMENT PRIMARY KEY,
     board_id BIGINT,
     product_code BIGINT,
-	image_link VARCHAR(20) NOT NULL,
+	image_link VARCHAR(100) NOT NULL,
     FOREIGN KEY (board_id) REFERENCES board (board_id) ON DELETE CASCADE,
     FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
 );
@@ -41,7 +41,7 @@ CREATE TABLE product_type (
 	product_type_id INT AUTO_INCREMENT PRIMARY KEY,
 	makeup_id BIGINT NOT NULL,
 	product_code BIGINT NOT NULL,
-	product_type_name VARCHAR(20) NOT NULL,
+	product_type_name VARCHAR(50) NOT NULL,
 	FOREIGN KEY (makeup_id) REFERENCES makeup (makeup_id) ON DELETE CASCADE,
 	FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
 );
@@ -49,7 +49,7 @@ CREATE TABLE product_type (
 CREATE TABLE comment (
 	comment_id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	board_id BIGINT NOT NULL,
-    member_id VARCHAR(20) NOT NULL,
+    member_id VARCHAR(50) NOT NULL,
     comment_content TEXT NOT NULL,
 	FOREIGN KEY (board_id) REFERENCES board (board_id) ON DELETE CASCADE,
 	FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE
