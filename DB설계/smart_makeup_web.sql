b7758fffc612d78d605ec28ec2dc38de4d3d5768
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-10-15 17:09:03 +0900

    DB/20241015/version_11
    데이터베이스 수정, 제품 종류를 제품과 화장품으로 변경하고 제품 테이블에는 제품 종류 속성을 추가

--- DB설계/smart_makeup_web.sql
+++ DB설계/smart_makeup_web.sql
@@ -1,4 +1,7 @@
-CREATE TABLE member (
+CREATE DATABASE smart_makeup;
+USE smart_makeup;
+
+CREATE TABLE member (	-- 사용자 테이블 
     member_id VARCHAR(50) NOT NULL UNIQUE,
 	member_password VARCHAR(100) NOT NULL,
     email VARCHAR(50) NOT NULL,
@@ -6,7 +9,7 @@ CREATE TABLE member (
     PRIMARY KEY(member_id)
 );
 
-CREATE TABLE makeup (
+CREATE TABLE makeup (	-- 화장 정보를 저장하기 위한 테이블 
 	makeup_id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	member_id VARCHAR(50),
 	color_code VARCHAR(20) NOT NULL,
@@ -14,7 +17,7 @@ CREATE TABLE makeup (
 	FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE
 );
 
-CREATE TABLE board (
+CREATE TABLE board (	-- 게시판을 저장하기 위한 테이블 
 	board_id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	member_id VARCHAR(50) NOT NULL,
     title VARCHAR(50) NOT NULL,
@@ -22,13 +25,14 @@ CREATE TABLE board (
 	FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE
 );
 
-CREATE TABLE product (
+CREATE TABLE product (	-- 광고를 위한 화장품 정보 테이블 
 	product_code BIGINT AUTO_INCREMENT PRIMARY KEY,
 	product_name VARCHAR(50) NOT NULL,
+    category VARCHAR(50) NOT NULL,
 	Price INT NOT NULL
 );
 
-CREATE TABLE image (
+CREATE TABLE image (	-- 이미지 링크를 위한 테이블 
 	image_code BIGINT AUTO_INCREMENT PRIMARY KEY,
     board_id BIGINT,
     product_code BIGINT,
@@ -37,16 +41,15 @@ CREATE TABLE image (
     FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
 );
 
-CREATE TABLE product_type (
-	product_type_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE product_makeup (	-- 화장 정보와 제품 정보를 위한 테이블 
+	product_makeup_id INT AUTO_INCREMENT PRIMARY KEY,
 	makeup_id BIGINT NOT NULL,
 	product_code BIGINT NOT NULL,
-	product_type_name VARCHAR(50) NOT NULL,
 	FOREIGN KEY (makeup_id) REFERENCES makeup (makeup_id) ON DELETE CASCADE,
 	FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE
 );
 
-CREATE TABLE comment (
+CREATE TABLE comment (	-- 댓글 저장을 위한 테이블 
 	comment_id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	board_id BIGINT NOT NULL,
     member_id VARCHAR(50) NOT NULL,
@@ -54,3 +57,27 @@ CREATE TABLE comment (
 	FOREIGN KEY (board_id) REFERENCES board (board_id) ON DELETE CASCADE,
 	FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE
 );
+
+-- 화장품 정보를 저장하기 위한 쿼리 
+INSERT INTO product (product_name, category, Price) VALUES ('estee lauder bone', 'fundation', 71930);
+INSERT INTO product (product_name, category, Price) VALUES ('estee lauder natural swade', 'fundation', 71930);
+INSERT INTO product (product_name, category, Price) VALUES ('estee lauder dun', 'fundation', 71930);
+INSERT INTO product (product_name, category, Price) VALUES ('estee lauder ecuru', 'fundation', 71930);
+INSERT INTO product (product_name, category, Price) VALUES ('estee lauder warm cream', 'fundation', 71930);
+INSERT INTO product (product_name, category, Price) VALUES ('locean vampire red', 'lipstick', 5090);
+INSERT INTO product (product_name, category, Price) VALUES ('locean red moon', 'lipstick', 5330);
+INSERT INTO product (product_name, category, Price) VALUES ('locean rose blossom', 'lipstick', 5330);
+INSERT INTO product (product_name, category, Price) VALUES ('locean dorothy red', 'lipstick', 15810);
+INSERT INTO product (product_name, category, Price) VALUES ('locean deep pink curten', 'lipstick', 5900);
+
+-- 이미지 테이블에 제품 이미지 링크 저장
+INSERT INTO image (product_code, image_link) VALUES (1, '/productimg/esteelauderbone.jpg');
+INSERT INTO image (product_code, image_link) VALUES (2, '/productimg/esteelaudernaturalswade.jpg');
+INSERT INTO image (product_code, image_link) VALUES (3, '/productimg/esteelauderdun.jpg');
+INSERT INTO image (product_code, image_link) VALUES (4, '/productimg/esteelauderecuru.jpg');
+INSERT INTO image (product_code, image_link) VALUES (5, '/productimg/esteelauderwarmcream.jpg');
+INSERT INTO image (product_code, image_link) VALUES (6, '/productimg/loceanvampirered.jpg');
+INSERT INTO image (product_code, image_link) VALUES (7, '/productimg/loceanredmoon.jpg');
+INSERT INTO image (product_code, image_link) VALUES (8, '/productimg/loceanroseblossom.jpg');
+INSERT INTO image (product_code, image_link) VALUES (9, '/productimg/loceandorothyred.jpg');
+INSERT INTO image (product_code, image_link) VALUES (10, '/productimg/loceandeeppinkcurten.jpg');
