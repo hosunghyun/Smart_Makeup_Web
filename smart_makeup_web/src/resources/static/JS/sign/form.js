581e73587ab24cd05b4bce51dc2e5c58da36f2a1
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-11-05 12:38:49 +0900

    Delete smart_makeup_web/src/resources directory

--- smart_makeup_web/src/resources/static/JS/sign/form.js
+++
@@ -1,5 +0,0 @@
-const $findid_form = document.getElementById("findid_form");    // 아이디를 찾기 위한 폼
-const $login_form = document.getElementById("login_form");      // 찾은 아이디를 출력하는 폼
-const $findpwd_form = document.getElementById("findpwd_form");  // 비밀번호 찾는 폼
-const $printid_form = document.getElementById("printid_form");  // 아이디 찾은 결과
-const $printpwd_form = document.getElementById("printpwd_form");    // 비밀번호 찾기에 성공했을 때 출력하는 폼
