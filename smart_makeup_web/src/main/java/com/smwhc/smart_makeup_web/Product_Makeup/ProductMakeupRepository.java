9a22be39ca2412d5f3b01a04d305b7f8b6d98c52
현호성 <149512530+hosunghyun@users.noreply.github.com>
2024-11-07 10:59:18 +0900

    smartmakeupweb/20241106/version_84

--- smart_makeup_web/src/main/java/com/smwhc/smart_makeup_web/Product_Makeup/ProductMakeupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smwhc.smart_makeup_web.Product_Makeup;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductMakeupRepository extends JpaRepository<ProductMakeup, Long>{
-    
-}
